ae7e0288aae8acc46199f47fe43828d7a211d93b
Joshua Booth <joshua.n.booth@gmail.com>
2019-01-07 15:05:14 -0800

    Tag system

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use std::collections::HashSet;
+extern crate regex;
 extern crate reqwest;
 extern crate select;
 extern crate serde;
@@ -6,10 +6,11 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+use regex::Regex;
 use reqwest::Client;
 use select::document::Document;
 use select::node::Node;
-use select::predicate::{Class, Name, Predicate, Text};
+use select::predicate::{Attr, Class, Name, Predicate, Text};
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 use std::collections::{HashSet, HashMap};
@@ -18,8 +19,31 @@ use std::fs::File;
 use std::cmp::Ordering;
 use std::str::FromStr;
 
-const SCRAPE: bool = false;
 const MAX_ARTICLE: u16 = 5000;
+
+fn ignore_tag(x: &str) -> bool {
+    [ "safe"
+    , "euclid"
+    , "keter"
+    , "thaumiel"
+    , "neutralized"
+    , "esoteric-class"
+    , "joke"
+    , "archived"
+    , "decommissioned"
+    , "neutralized"
+    , "supplement"
+    , "experiment"
+    , "collaboration"
+    , "exploration"
+    , "incident"
+    , "interview"
+    , "tale"
+    , "collaboration"
+    , "alive"
+    , "sentient"
+    ].contains(&x)
+}
 
 type IO<T> = Result<T, Box<Error>>;
 
@@ -27,8 +51,9 @@ type IO<T> = Result<T, Box<Error>>;
 struct Article {
     number: u16,
     title: String,
-    up: HashSet<u32>,
-    down: HashSet<u32>
+    tags: HashSet<u16>,
+    up: HashSet<u16>,
+    down: HashSet<u16>
 }
 
 #[derive(Clone, Deserialize, Serialize)]
@@ -38,13 +63,20 @@ struct Suggestions {
     xs: Vec<[usize; 2]>
 }
 
+#[derive(Clone, Deserialize, Serialize)]
+struct Cube {
+    number: u16,
+    title: String,
+    cube: String
+}
+
 impl Article {
     fn simil(&self, other: &Article) -> f64 {
         let x_up = &self.up;
         let x_down = &self.down;
-        let x_mag = (x_up | x_down).len() as f64;
         let y_up = &other.up;
         let y_down = &other.down;
+        let x_mag = (x_up | x_down).len() as f64;
         let y_mag = (y_up | y_down).len() as f64;
         let pos = (x_up & y_up).len() as f64;
         let neg = (&(x_up & y_down) | &(x_down & y_up)).len() as f64;
@@ -61,21 +93,21 @@ impl Article {
     }
 }
 
-struct Users {
-    i: u32,
-    users: HashMap<String, u32>
+struct Indexer {
+    i: u16,
+    db: HashMap<String, u16>
 }
-impl Users {
-    pub fn new() -> Users {
-        Users { i: 0, users: HashMap::new() }
+impl Indexer {
+    pub fn new() -> Indexer {
+        Indexer { i: 0, db: HashMap::new() }
     }
-    pub fn get(&mut self, name: String) -> u32 {
-        match self.users.get(&name) {
-            Some(&user) => user,
+    pub fn get(&mut self, k: String) -> u16 {
+        match self.db.get(&k) {
+            Some(&v) => v,
             None => {
                 let i = self.i;
                 self.i += 1;
-                self.users.insert(name, i);
+                self.db.insert(k, i);
                 i
             }
         }
@@ -118,8 +150,45 @@ fn parse_id(doc: &str) -> Result<u32, <u32 as FromStr>::Err> {
     }.unwrap_or("").parse()
 }
 
-/// Obtains upvoters and downvoters from a webpage and converts them into an Article.
-fn parse_votes(users: &mut Users, number: u16, title: String, doc: Document) -> Article {
+fn parse_tags(tags: &mut Indexer, doc: &Document) -> HashSet<u16> {
+    doc
+        .find(Class("page-tags").child(Name("a")))
+        .map(|x| x.text())
+        .filter(|x| !ignore_tag(x))
+        .map(|x| tags.get(x))
+        .collect()
+}
+
+// The first thousand instances of "1 x 1 x 1" etc.
+fn cube_queries() -> Vec<Regex> {
+    (1..1000)
+        .filter_map(|x| Regex::new(&format!("({}['\"]*\\s*[xX]\\s*{}['\"]*\\s*[xX]\\s*{}I)", x, x, x)).ok())
+        .collect()
+}
+
+// Looks for "1 x 1 x 1" etc.
+fn find_cube(queries: &Vec<Regex>, doc: &Document) -> Option<String> {
+    let mut active = false;
+    for node in doc.find(Attr("id", "page-content").child(Name("p"))) {
+        let text = node.text();
+        if text.contains("Special Containment Procedures:") {
+            active = true;
+        } else if text.contains("Description:") {
+            active = false;
+        }
+        if active {
+            for query in queries {
+                for cap in query.captures_iter(&text) {
+                    return Some(cap[0].to_string())
+                }
+            }
+        }
+    }
+    None
+}
+
+/// (upvoters, downvoters) from a webpage.
+fn parse_votes(users: &mut Indexer, doc: Document) -> (HashSet<u16>, HashSet<u16>) {
     let mut up = HashSet::new();
     let mut down = HashSet::new();
     for node in doc.find(Name("a")) {
@@ -139,7 +208,7 @@ fn parse_votes(users: &mut Users, number: u16, title: String, doc: Document) -> 
             }
         }
     }
-    Article { number, title, up, down }
+    (up, down)
 }
 
 /// Requests names for all articles from the mainlist.
@@ -175,15 +244,55 @@ fn suggest(xs: &[Article], x: &Article) -> Suggestions {
     Suggestions { i: x.number, s: x.title.clone(), xs: suggestions }
 }
 
-/// Queries Wikidot for information about an article.
-fn request_article(client: &Client, users: &mut Users, number: u16, title: String) -> IO<Article> {
+/// Queries Wikidot for an article's HtML.
+fn request_page(client: &Client, number: u16) -> IO<Document> {
+    let scp = format!("SCP-{:03}", number);
+    println!("Scraping: {}", scp);
     let res = client
-        .get(&format!("http://scp-wiki.wikidot.com/SCP-{:03}", number))
-        .send()?
-        .text()?;
-    let id = parse_id(&res)?;
-    let doc = request_module(&client, "pagerate/WhoRatedPageModule", id)?;
-    Ok(parse_votes(users, number, title, doc))
+        .get(&format!("http://scp-wiki.wikidot.com/{}", scp))
+        .send()?;
+    Ok(Document::from_read(res)?)
+}
+
+fn any_matches(queries: &Vec<Regex>, x: &str) -> bool {
+    for query in queries {
+        if query.is_match(x) {
+            return true;
+        }
+    }
+    false
+}
+
+fn request_cubes(client: &Client) -> IO<Vec<Cube>> {
+    let titles = record_titles(&client)?;
+    let len = titles.len();
+    let queries = cube_queries();
+    for x in &["5 x 5 x 5", "5x5x5", "3\" x 3\" x 3\"", "5X5 x 5"] {
+        println!("{} matches: {}", x, any_matches(&queries, &x))
+    }
+    let mut cubes = Vec::new();
+    for (number, title) in titles {
+        match request_page(&client, number).ok().and_then(|page| find_cube(&queries, &page)) {
+            None => (),
+            Some(cube) => {
+                println!("SCP-{} ({}): {}", number, title, cube);
+                cubes.push(Cube { number, title, cube })
+            }
+        }
+    }
+    println!("{} / {} ({:.2}%)", cubes.len(), len, cubes.len() as f64 * 100.0 / len as f64);
+    Ok(cubes)
+}
+
+/// Queries Wikidot for information about an article.
+fn request_article(client: &Client, users: &mut Indexer, tag: &mut Indexer, number: u16, title: String) -> IO<Article> {
+    let page = request_page(&client, number)?;
+    let text = page.nth(0).unwrap().inner_html();
+    let id = parse_id(&text)?;
+    let rated = request_module(&client, "pagerate/WhoRatedPageModule", id)?;
+    let tags = parse_tags(tag, &page);
+    let (up, down) = parse_votes(users, rated);
+    Ok(Article { number, title, tags, up, down })
 }
 
 /// POSTs a request to Wikidot's AJAX module connector.
@@ -207,12 +316,12 @@ fn write_json<T: Serialize>(path: &str, data: &T) -> IO<()> {
     Ok(serde_json::to_writer(file, data)?)
 }
 
-fn record_votes(client: &Client, users: &mut Users, titles: HashMap<u16, String>) -> IO<()> {
+fn record_votes(client: &Client, users: &mut Indexer, tags: &mut Indexer, titles: HashMap<u16, String>) -> IO<()> {
     let mut articles = Vec::new();
     for i in 2..MAX_ARTICLE {
         let scp = format!("SCP-{:03}", i);
         let title = titles.get(&i).unwrap_or(&scp).to_string();
-        if let Ok(article) = request_article(&client, users, i, title) {
+        if let Ok(article) = request_article(&client, users, tags, i, title) {
             println!("Loading: {}", scp);
             articles.push(article);
         }
@@ -221,11 +330,18 @@ fn record_votes(client: &Client, users: &mut Users, titles: HashMap<u16, String>
     write_json("data/articles.json", &articles)
 }
 
+fn record_cubes() -> IO<()> {
+    let client = Client::new();
+    let cubes = request_cubes(&client)?;
+    write_json("data/cubes.json", &cubes)
+}
+
 fn scrape() -> IO<()> {
-    let client = reqwest::Client::new();
-    let mut users = Users::new();
+    let client = Client::new();
+    let mut users = Indexer::new();
+    let mut tags = Indexer::new();
     let titles = record_titles(&client)?;
-    record_votes(&client, &mut users, titles)
+    record_votes(&client, &mut users, &mut tags, titles)
 }
 
 fn suggests() -> IO<()> {
@@ -238,8 +354,19 @@ fn suggests() -> IO<()> {
     write_json("data/suggestions.json", &suggestions)
 }
 
+fn scratch() -> Result<String, reqwest::Error> {
+    Client::new()
+        .post("http://scp-wiki.wikidot.com/ajax-module-connector.php")
+        .form(&[
+            ("moduleName", "list/ListPagesModule"),
+            ("fullname", "SCP-3209")
+        ])
+        .send()?
+        .text()
+}
+
 fn main() {
-    match if SCRAPE { scrape() } else { suggests() } {
+    match { scrape().and_then(|_| suggests()) } {
         Err(err) => println!("{}", err),
         Ok(_) => ()
     }
